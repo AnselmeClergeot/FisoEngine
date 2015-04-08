6e25e0e221c79e5d7e5467e47dd7fc6a33e01f4c
Anselme Clergeot <anselmedu25@hotmail.com>
2015-04-08 20:29:20 +0200

    Update main.cpp

--- Examples/SFML Demo/Sources files/main.cpp
+++ Examples/SFML Demo/Sources files/main.cpp
@@ -1,3 +1,5 @@
+/* This demo shows map initialization, shadows setting and map entity adding. Feel free to modify.*/
+
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
@@ -8,50 +10,40 @@ int main()
 {
     RunEnvironment environment;
     environment.setWindowResolution(800, 600);
+	
+    Map myMap(environment);
 
-    Map carte(environment);
+    myMap.addLayerFile("layer1.map");
+    myMap.addLayerFile("layer2.map");
+    myMap.addLayerFile("layer3.map");
+    myMap.addLayerFile("layer4.map");
 
-    carte.addLayerFile("layer1.map");
-    carte.addLayerFile("layer2.map");
-    carte.addLayerFile("layer3.map");
-    carte.addLayerFile("layer4.map");
+    myMap.setTileset("texture.png");
 
-    carte.setTileset("texture.png");
+    myMap.setTileSize(Vector2(64, 72));
+    myMap.setTileBaseHeight(32);
 
-    carte.setTileSize(Vector2(64, 72));
-    carte.setTileBaseHeight(32);
+    myMap.setPosition(300, 100);
 
-    carte.setPosition(300, 100);
+    myMap.init();
 
-    carte.init();
+    myMap.shadows().setTileset("shadowsTex.png");
+    myMap.shadows().init();
+    myMap.shadows().setOpacity(130);
+    myMap.shadows().setOn(true);
 
-    carte.shadows().setTileset("shadowsTex.png");
-    carte.shadows().init();
-    carte.shadows().setOpacity(130);
-    carte.shadows().setOn(true);
+    myMap.setInvisibleTile(0);
 
-    carte.setInvisibleTile(0);
+    myMap.entities().createEntity();
+    myMap.entities().entityAt(0).setImagePath("entity.png");
+    myMap.entities().entityAt(0).setLayer(1);
+    myMap.entities().entityAt(0).setDimensions(21, 44);
+    myMap.entities().entityAt(0).setBaseCoord(10, 44);
+    myMap.entities().entityAt(0).updatePixelPosition(180, 140);
+    myMap.entities().entityAt(0).setDrawState(true);
 
     sf::RenderWindow window(sf::VideoMode(800, 600), "[SFML] FisoEngine entities + shadows demo");
     window.setFramerateLimit(60);
-
-    carte.entities().createEntity();
-    carte.entities().setImagePathOf(0, "entity.png");
-    carte.entities().setDimensionsOf(0, Vector2(21, 44));
-    carte.entities().setBaseCoordOf(0, Vector2(10, 43));
-    carte.entities().setLayerOf(0, 1);
-    carte.entities().updatePixelPositionOf(0, Vector2(315, 180));
-    carte.entities().setDrawStateOf(0, true);
-
-    carte.entities().createEntity();
-    carte.entities().setImagePathOf(1, "entity.png");
-    carte.entities().setDimensionsOf(1, Vector2(21, 44));
-    carte.entities().setBaseCoordOf(1, Vector2(10, 43));
-    carte.entities().setLayerOf(1, 1);
-    carte.entities().updatePixelPositionOf(1, Vector2(260, 179));
-    carte.entities().setDrawStateOf(1, true);
-
-    carte.entities().getSpriteOf(1).setColor(sf::Color(255, 0, 0, 128));
 
     while (window.isOpen())
     {
@@ -60,36 +52,11 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
-
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-                    carte.entities().move(0, Vector2(-2, -1));
-
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-                    carte.entities().move(0, Vector2(2, 1));
-
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-                    carte.entities().move(0, Vector2(-2, 1));
-
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-                    carte.entities().move(0, Vector2(2, -1));
-
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
-                    carte.entities().move(1, Vector2(-2, -1));
-
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-                    carte.entities().move(1, Vector2(2, 1));
-
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-                    carte.entities().move(1, Vector2(-2, 1));
-
-                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
-                    carte.entities().move(1, Vector2(2, -1));
         }
 
         window.clear(sf::Color(255, 128, 255, 255));
-        window.draw(carte);
+        window.draw(myMap);
         window.display();
     }
-
     return 0;
 }
