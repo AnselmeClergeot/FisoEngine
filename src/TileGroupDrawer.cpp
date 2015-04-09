b0ea7c19f5d118a46ba3cb5075d8ec8630bcf3d8
Anselme Clergeot <anselmedu25@hotmail.com>
2015-04-09 16:58:58 +0200

    Performance amelioration in tile drawing

--- src/TileGroupDrawer.cpp
+++ src/TileGroupDrawer.cpp
@@ -13,13 +13,14 @@ void TileGroupDrawer::draw(sf::RenderTarget& target, const unsigned int layer,
     for(int x(0); x<m_mapData.getSize().x; x++)
         for(int y(0); y<m_mapData.getSize().x; y++)
     {
-        if(isVisible(toIsometricPosition(Vector3(x, y, layer), m_mapData),
-                             m_mapData.getTileSize(), m_environment))
+        if(isVisible(toIsometricPosition(Vector3(x, y, layer), m_mapData),m_mapData.getTileSize(), m_environment) &&
+           m_mapData.getTempConf().at(x, y, layer)!=m_mapData.getInvisibleTile() &&
+           m_data.getTileOpacity(m_mapData.getTempConf().get3dIter(x, y, layer))>0)
         {
             target.draw(m_data.spriteAt(Vector3(x, y, layer)));
         }
 
-        //If the tilegroup is a map tilegroup and that entities have to be interpose, then
+        //If the tilegroup is a map tilegroup and that entities have to be interpose, then interpose
         if(interposing!=0)
         {
             interposing->interpose(Vector3(x, y, layer), target);
