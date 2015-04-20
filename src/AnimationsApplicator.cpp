37ac9c874274b6875cb7388d0db9a5625a04b317
Anselme Clergeot <anselmedu25@hotmail.com>
2015-04-20 07:12:51 +0200

    Delete AnimationsApplicator.cpp

--- src/AnimationsApplicator.cpp
+++
@@ -1,7 +0,0 @@
-#include "AnimationsApplicator.h"
-
-AnimationsApplicator::AnimationsApplicator(TileGroup &mapTilegroup, TileGroup &shadowsTilegroup, Animator &animator) :
-m_mapTilegroup(mapTilegroup), m_shadowsTilegroup(shadowsTilegroup), m_animator(animator)
-{
-
-}
